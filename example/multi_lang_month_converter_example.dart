791031f9c47b1d9aca5d391ae74e6b3b9cec4fee
Akmaljon Abdirakhimov <AkmaljonAbdirakhimov@gmail.com>
2024-08-04 22:36:31 +0500

    examples added

--- example/multi_lang_month_converter_example.dart
+++ example/multi_lang_month_converter_example.dart
@@ -1,3 +1,9 @@
 import 'package:multi_lang_month_converter/multi_lang_month_converter.dart';
 
-void main() {}
+void main() {
+  final converter = MultiLangMonthConverter();
+
+  print(converter.convert(1, 'uz')); // Yanvar
+  print(converter.convert(1, 'ru')); // Январь
+  print(converter.convert(1, 'en')); // January
+}
